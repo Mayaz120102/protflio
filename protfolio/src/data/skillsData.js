9f1fdfbccbf7b53eab4dd7f4a31f21afddb62e7d
Abrar Mayaz <140696838+Mayaz120102@users.noreply.github.com>
2026-08-11 20:23:48 +0600

    updated skills

--- protfolio/src/data/skillsData.js
+++ protfolio/src/data/skillsData.js
@@ -30,13 +30,13 @@ export const skillsData = [
     image:
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
   },
-  {
-    id: 5,
-    name: "Node.js",
-    category: "Frameworks",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-  },
+  // {
+  //   id: 5,
+  //   name: "Node.js",
+  //   category: "Frameworks",
+  //   image:
+  //     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  // },
   {
     id: 6,
     name: "SQL",
@@ -70,6 +70,20 @@ export const skillsData = [
     image:
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
   },
+  {
+    id: 11,
+    name: "FastApi",
+    category:"Framworks",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg",
+  },
+  {
+    id: 12,
+    name: "ExpressJS",
+    category:"Framworks",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
+  }
 ];
 
 export const categories = [
