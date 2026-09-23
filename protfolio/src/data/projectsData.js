93631d5988687ebe2655d31ce1126a3b0bc0e2ed
Mayaz120102 <abrarmayaz2002@gmail.com>
2026-09-23 13:00:27 +0600

    projectss update

--- protfolio/src/data/projectsData.js
+++ protfolio/src/data/projectsData.js
@@ -1,30 +1,55 @@
-
 export const projectsData = [
   {
     id: 1,
-    title: 'Job Portal',
-    description: 'An AI based CareerP, this was our hackathon task where i did the frontend part, that was an 24 hours hackathon',
-    image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?w=800&h=600&fit=crop',
-    link: 'https://github.com/Mayaz120102/hackathon-frontend',
-    technologies: ['Reactjs', 'Django DRF', 'Node.js'],
+    title: "Job Portal",
+    description:
+      "An AI based CareerP, this was our hackathon task where i did the frontend part, that was an 24 hours hackathon",
+    image:
+      "https://images.unsplash.com/photo-1551288049-bebda4e38f71?w=800&h=600&fit=crop",
+    link: "https://github.com/Mayaz120102/hackathon-frontend",
+    technologies: ["Reactjs", "Django DRF", "Node.js"],
   },
   {
     id: 2,
-    title: 'eGoru Hat',
+    title: "eGoru Hat",
     description:
       "A project I built right after learning Express.js fundamentals through a CRUD practice project. The idea came to me during Eid-ul-Azha, and I vibe-coded it into a working full-stack app to put what I'd just learned into practice.",
-    image: '/projects/egoru.png', 
-    link: 'https://egoru-frontend.vercel.app/',
-    GithubLink: 'https://github.com/Mayaz120102/express_learning/tree/main/CRUD_practice/eGoru',
-    technologies: ['React', 'Express.js', 'Node.js', 'MongoDb'],
+    image: "/projects/egoru.png",
+    link: "https://egoru-frontend.vercel.app/",
+    GithubLink:
+      "https://github.com/Mayaz120102/express_learning/tree/main/CRUD_practice/eGoru",
+    technologies: ["React", "Express.js", "Node.js", "MongoDb"],
   },
   {
     id: 3,
-    title: 'Expense Tracker API',
-    description: 'A expense Tracker backend Api project that can store your expense and income information build with FastAPI, you can add, edit, read and delete your expense or income',
-    image: '/projects/expense.jpg',
+    title: "Expense Tracker API",
+    description:
+      "A expense Tracker backend Api project that can store your expense and income information build with FastAPI, you can add, edit, read and delete your expense or income",
+    image: "/projects/expense.jpg",
     link: "https://expense-tracker-api-6x8q.onrender.com",
-    Githublink: 'https://github.com/Mayaz120102/expense_tracker_api',
-    technologies: ['FastAPI', 'Supabase'],
+    Githublink: "https://github.com/Mayaz120102/expense_tracker_api",
+    technologies: ["FastAPI", "Supabase"],
+  },
+  {
+    id: 4,
+    title: "Job_tracker",
+    description:
+      "A job tracker webapp where users can track there job application. They can add applied jobs as well as pending, rejected, interview one with date, they also can upload there resume accroding to the job",
+    image: "/projects/Job_tracker.png",
+    link: "https://job-tracker-frontend-taupe-delta.vercel.app",
+    GithubLink1: "https://github.com/Mayaz120102/job-tracker-backend.git",
+    GithubLink2: "https://github.com/Mayaz120102/job-tracker-frontend.git",
+    technologies: ["FastAPI", "Supabase", "React"],
+  },
+  {
+    id: 5,
+    title: "RoktoShetu-BloodApp",
+    description:
+      "A Blood-connect app where rquester can requester for blood, donor can apply for donate, hospital can fullfill request by adding stocks, admin can verfity or ban users",
+    image: "/projects/RoktoShetu.png",
+    link: "https://roktoshetu-frontend-v1.vercel.app",
+    GithubLink1: "https://github.com/Mayaz120102/rocktoshetu-backend.git",
+    GithubLink2: "https://github.com/Mayaz120102/roktoshetu-frontend-v1.git",
+    technologies: ["FastAPI", "Supabase", "React"],
   },
 ];
